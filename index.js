90044173270c76bdeda5a3e3f76837070e882877
xJohnnyy <152817770+xJohnnyy@users.noreply.github.com>
2024-03-06 16:12:08 -0600

    updated

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ class Svg{
 
         return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
     }
+
     setTextElement(text,color){
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
     }
+    
     setShapeElement(shape){
         this.shapeElement = shape.render()
 
