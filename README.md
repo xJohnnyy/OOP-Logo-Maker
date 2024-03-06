ba5a680ce5594ed96ccc29d13fe540fffd94376f
xJohnnyy <152817770+xJohnnyy@users.noreply.github.com>
2024-03-06 14:34:53 -0600

    new test

---
+++ README.md
@@ -0,0 +1,36 @@
+# OOP Logo Maker
+
+  
+## Description:
+The application enables users to enter inputs into a inquirer prompt in order to generate a logo "logo.svg" file which contains the users desired results as an SVG image.
+
+## Usage Instructions
+1. Open the cloned repository in any source code editor e.g. Visual Studio Code.
+2. Open integrated terminal on index.js
+3. Enter “node index.js” in the command line
+4. Through sequential order within the command line interface - the user will be presented with npm inquirer questions - the user will provide a response to each question and proceed.
+5. Once completed a SVG file named "logo.svg" will be created.
+6. At your discretion, you may rename the "logo.svg" file to the file name of your choice.
+7. (Optional) Regarding future use, you may alter the prompted questions within the index.js to best suit your needs.
+8. (Optional) The SVG Logo Maker serves as a foundation so you are not limited to altering the files (as you have my permission) to create the best possible "logo.svg" file.
+
+
+## Walkthrough Video:
+[Click Here to Watch]
+
+## Screenshots:
+### Figure 1. Command line application
+![](./images/svg-logo-maker.png) 
+### Figure 2. Jest Testing "npm test"
+![](./images/jest-test.png)
+### Figure 3. Generated "logo.svg" file
+![](./images/generated-logo.svg.png)
+
+## Installation Process
+1. Clone the repository: [Johnny's OOP Logo Maker](My repo Link)
+2. Install the following: 
+- Node.JS (https://nodejs.org/en/blog/release/v16.18.1/)
+- Jest (https://www.npmjs.com/package/jest)
+- Inquirer.js: (https://www.npmjs.com/package/inquirer/v/8.2.4)
+3. Open the cloned repository in any source code editor.
+4. Open the integrated terminal for the document and complete the respective installation guides provided above in section (2.) to ensure the cloned documentation will operate.
---
+++ index.js
@@ -0,0 +1,113 @@
+const filesystem = require('./node_modules/graceful-fs/graceful-fs')
+const inquirer = require("inquirer");
+const {Circle, Square, Triangle} = require("./lib/shapes");
+
+class Svg{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+
+    }
+    
+}
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "TEXT: Enter up to (3) Characters:",
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "TEXT COLOR: Enter a color keyword (OR a hexadecimal number):",
+    },
+    {
+        type: "input",
+        name: "shape",
+        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):",
+    },
+    {
+        type: "list",
+        name: "pixel-image",
+        message: "Choose which Pixel Image you would like?",
+        choices: ["Circle", "Square", "Triangle"],
+    },
+];
+
+function writeToFile(fileName, data) {
+	console.log("Writing [" + data + "] to file [" + fileName + "]")
+    filesystem.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Congratulations, you have Generated a logo.svg!");
+    });
+}
+
+async function init() {
+    console.log("Starting init");
+	var svgString = "";
+	var svg_file = "logo.svg";
+
+    const answers = await inquirer.prompt(questions);
+
+	var user_text = "";
+	if (answers.text.length > 0 && answers.text.length < 4) {
+		user_text = answers.text;
+	} else {
+	
+		console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less");
+        return;
+	}
+	console.log("User text: [" + user_text + "]");
+
+	user_font_color = answers["text-color"];
+	console.log("User font color: [" + user_font_color + "]");
+	
+	user_shape_color = answers.shape;
+	console.log("User shape color: [" + user_shape_color + "]");
+	
+	user_shape_type = answers["pixel-image"];
+	console.log("User entered shape = [" + user_shape_type + "]");
+	
+	let user_shape;
+	if (user_shape_type === "Square" || user_shape_type === "square") {
+		user_shape = new Square();
+		console.log("User selected Square shape");
+	}
+	else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+		user_shape = new Circle();
+		console.log("User selected Circle shape");
+	}
+	else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+		user_shape = new Triangle();
+		console.log("User selected Triangle shape");
+	}
+	else {
+		console.log("Invalid shape!");
+	}
+	user_shape.setColor(user_shape_color);
+
+	var svg = new Svg();
+	svg.setTextElement(user_text, user_font_color);
+	svg.setShapeElement(user_shape);
+	svgString = svg.render();
+	
+	console.log("Displaying shape:\n\n" + svgString);
+
+	console.log("Shape generation complete!");
+	console.log("Writing shape to file...");
+	writeToFile(svg_file, svgString); 
+}
+init()
